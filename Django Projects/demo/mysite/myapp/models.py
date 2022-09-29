ec2adbecc590e8c7274d5cb6f577ede38c7f38fb
hadas8 <87123641+hadas8@users.noreply.github.com>
2022-09-29 12:10:30 +0300

    Add files via upload

---
+++ Django Projects/demo/mysite/myapp/models.py
@@ -0,0 +1,16 @@
+from distutils.command.upload import upload
+from email.policy import default
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+
+    def __str__(self) -> str:
+        return self.name
+    
+    name = models.CharField(max_length=100)
+    desc = models.CharField(max_length=300)
+    price = models.IntegerField()
+    book_image = models.ImageField(default='default.jpg', upload_to='book_images/')
